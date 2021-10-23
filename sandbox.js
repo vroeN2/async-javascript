841568ff8e41c4d0aa797d72407d7b536e22e41e
Maciej Zachariasz <47660926+vroeN2@users.noreply.github.com>
2021-10-23 08:46:30 +0200

    lesson 03 final files
    improve the function to not only check request state, but also request status and work around it

--- sandbox.js
+++ sandbox.js
@@ -2,8 +2,10 @@ const request = new XMLHttpRequest();
 
 request.addEventListener('readystatechange', () => {
     // console.log(request, request.readyState);
-    if (request.readyState === 4) {
+    if (request.readyState === 4 && request.status === 200) {
         console.log(request.responseText);
+    } else if (request.readyState === 4) {
+        console.log('could not fetch the data.')
     }
 });
 

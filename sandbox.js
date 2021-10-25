469a7798233cd5e99c413e38ac2411b2511b2d5e
Maciej Zachariasz <47660926+vroeN2@users.noreply.github.com>
2021-10-25 09:38:12 +0200

    lesson 6 final files
    learn how a callback hell looks like

--- sandbox.js
+++ sandbox.js
@@ -1,4 +1,4 @@
-const getTodos = callback => {
+const getTodos = (resource, callback) => {
     const request = new XMLHttpRequest();
 
     request.addEventListener('readystatechange', () => {
@@ -11,15 +11,16 @@ const getTodos = callback => {
     });
 
     // request.open('GET', 'https://jsonplaceholder.typicode.com/todos/');
-    request.open('GET', 'todos.json');
+    request.open('GET', resource);
     request.send();
 };
 
-getTodos((err, data) => {
-    console.log('callback fired');
-    if (err) {
-        console.log(err);
-    } else {
+getTodos('todos/luigi.json', (err, data) => {
+    console.log(data);
+    getTodos('todos/mario.json', (err, data) => {
         console.log(data);
-    }
+        getTodos('todos/shaun.json', (err, data) => {
+            console.log(data);
+        })
+    })
 });

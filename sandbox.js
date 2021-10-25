bcc7b3482bb0dca8ccec0d85923efb2d8b885d71
Maciej Zachariasz <47660926+vroeN2@users.noreply.github.com>
2021-10-25 16:05:53 +0200

    lesson 11 final files
    throw and catch of an error in asynchronous code

--- sandbox.js
+++ sandbox.js
@@ -2,27 +2,18 @@
 
 const getTodos = async () => {
 
-    const response = await fetch('todos/luigi.json');
+    const response = await fetch('todos/luigia.json');
+
+    if (response.status !== 200) {
+        throw new Error('cannot fetch the data');
+    }
+
     const data = await response.json();
     
     return data;
 
 };
 
-console.log(1);
-console.log(2);
 getTodos()
-    .then(data => console.log('resolved', data));
-
-console.log(3);
-console.log(4);
-// // fetch api
-
-// fetch('todos/luigi.json').then(response => {
-//     console.log('resolved', response);
-//     return response.json();
-// }).then(data => {
-//   console.log('data: ', data);
-// }).catch(err => {
-//     console.log('rejected', err);
-// });
+    .then(data => console.log('resolved:', data))
+    .catch(err => console.log('rejected:', err.message));

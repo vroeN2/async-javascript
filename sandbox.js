2b95ae963da87e54f03f9cec3013572594e0bea8
Maciej Zachariasz <47660926+vroeN2@users.noreply.github.com>
2021-10-25 15:24:46 +0200

    lesson 9 final files
    fetch API, how to display response and data of fetch api

--- sandbox.js
+++ sandbox.js
@@ -1,54 +1,10 @@
-const getTodos = (resource) => {
+// fetch api
 
-    return new Promise((resolve, reject) => {
-        const request = new XMLHttpRequest();
-
-        request.addEventListener('readystatechange', () => {
-            if (request.readyState === 4 && request.status === 200) {
-                const data = JSON.parse(request.responseText);
-                resolve(data);
-            } else if (request.readyState === 4) {
-                reject('error getting resource');
-            }
-        });
-
-        request.open('GET', resource);
-        request.send();
-    })
-};
-
-getTodos('todos/luigi.json').then(data => {
-    console.log('promise 1 resolved: ', data);
-    return getTodos('todos/mario.json')
+fetch('todos/luigi.json').then(response => {
+    console.log('resolved', response);
+    return response.json();
 }).then(data => {
-    console.log('promise 2 resolved: ', data);
-    return getTodos('todos/shaun.json')
-}).then(data => {
-    console.log('promise 3 resolved: ', data);
+  console.log('data: ', data);
 }).catch(err => {
-    console.log('promise rejected: ', err);
-})
-
-// promise example
-
-const getSomething = () => {
-
-    return new Promise((resolve, reject) => {
-        // fetch something
-        resolve('some data');
-        // reject('some error');
-    });
-
-};
-
-// getSomething().then((data) => {
-//     console.log(data);
-// }, (err) => {
-//     console.log(err);
-// })
-
-// getSomething().then(data => {
-//     console.log(data);
-// }).catch(err => {
-//     console.log(err);
-// })
+    console.log('rejected', err);
+});

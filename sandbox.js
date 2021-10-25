430a2a3b4c05c340da50cd749b7a927217082390
Maciej Zachariasz <47660926+vroeN2@users.noreply.github.com>
2021-10-25 10:08:15 +0200

    lesson 7 final files
    learn promises and how to use them

--- sandbox.js
+++ sandbox.js
@@ -1,26 +1,48 @@
-const getTodos = (resource, callback) => {
-    const request = new XMLHttpRequest();
+const getTodos = (resource) => {
 
-    request.addEventListener('readystatechange', () => {
-        if (request.readyState === 4 && request.status === 200) {
-            const data = JSON.parse(request.responseText);
-            callback(undefined, data);
-        } else if (request.readyState === 4) {
-            callback('could not fetch data', undefined);
-        }
-    });
+    return new Promise((resolve, reject) => {
+        const request = new XMLHttpRequest();
 
-    // request.open('GET', 'https://jsonplaceholder.typicode.com/todos/');
-    request.open('GET', resource);
-    request.send();
+        request.addEventListener('readystatechange', () => {
+            if (request.readyState === 4 && request.status === 200) {
+                const data = JSON.parse(request.responseText);
+                resolve(data);
+            } else if (request.readyState === 4) {
+                reject('error getting resource');
+            }
+        });
+
+        request.open('GET', resource);
+        request.send();
+    })
 };
 
-getTodos('todos/luigi.json', (err, data) => {
-    console.log(data);
-    getTodos('todos/mario.json', (err, data) => {
-        console.log(data);
-        getTodos('todos/shaun.json', (err, data) => {
-            console.log(data);
-        })
-    })
-});
+getTodos('todos/luigi.json').then(data => {
+    console.log('promise resolved: ', data);
+}).catch(err => {
+    console.log('promise rejected: ', err);
+})
+
+// promise example
+
+const getSomething = () => {
+
+    return new Promise((resolve, reject) => {
+        // fetch something
+        resolve('some data');
+        // reject('some error');
+    });
+
+};
+
+// getSomething().then((data) => {
+//     console.log(data);
+// }, (err) => {
+//     console.log(err);
+// })
+
+// getSomething().then(data => {
+//     console.log(data);
+// }).catch(err => {
+//     console.log(err);
+// })

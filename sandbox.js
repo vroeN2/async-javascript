e1c4b6799ed7f89aab6b2a7af249fb70af203ae4
Maciej Zachariasz <47660926+vroeN2@users.noreply.github.com>
2021-10-25 16:01:42 +0200

    lesson 10 final files
    learn about async and await

--- sandbox.js
+++ sandbox.js
@@ -1,10 +1,28 @@
-// fetch api
+// async & await
 
-fetch('todos/luigi.json').then(response => {
-    console.log('resolved', response);
-    return response.json();
-}).then(data => {
-  console.log('data: ', data);
-}).catch(err => {
-    console.log('rejected', err);
-});
+const getTodos = async () => {
+
+    const response = await fetch('todos/luigi.json');
+    const data = await response.json();
+    
+    return data;
+
+};
+
+console.log(1);
+console.log(2);
+getTodos()
+    .then(data => console.log('resolved', data));
+
+console.log(3);
+console.log(4);
+// // fetch api
+
+// fetch('todos/luigi.json').then(response => {
+//     console.log('resolved', response);
+//     return response.json();
+// }).then(data => {
+//   console.log('data: ', data);
+// }).catch(err => {
+//     console.log('rejected', err);
+// });
